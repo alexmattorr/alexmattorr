b2986a396967107024a502edac12fb0d565042f5
Alex <alexmattorr@gmail.com>
2015-07-27 22:07:52 -0700

    working in tempalates into php

--- wp-content/themes/alexmattorr/home.php
+++ wp-content/themes/alexmattorr/home.php
@@ -1,8 +1,10 @@
 <?php
 /* Template Name: Home Page Template */
 get_header();?>
-<?php include 'templates/top.php';?>
-<?php include 'templates/about.php';?>
-<?php include 'templates/work.php';?>
-<?php include 'templates/contact.php';?>
+<div class="container">
+	<?php include 'templates/top.php';?>
+	<?php include 'templates/about.php';?>
+	<?php include 'templates/work.php';?>
+	<?php include 'templates/contact.php';?>
+</div>
 <?php get_footer();?>
