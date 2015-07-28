3c9768a1faee9845a25c9a0fc538c0c2456fbb60
Alex <alexmattorr@gmail.com>
2015-07-27 22:29:28 -0700

    work wordpress loop

--- wp-content/themes/alexmattorr/home.php
+++ wp-content/themes/alexmattorr/home.php
@@ -1,10 +1,10 @@
 <?php
 /* Template Name: Home Page Template */
 get_header();?>
-<div class="container">
-	<?php include 'templates/top.php';?>
-	<?php include 'templates/about.php';?>
-	<?php include 'templates/work.php';?>
-	<?php include 'templates/contact.php';?>
-</div>
+	<div class="container">
+		<?php include 'templates/top.php';?>
+		<?php include 'templates/about.php';?>
+		<?php include 'templates/work.php';?>
+		<?php include 'templates/contact.php';?>
+	</div>
 <?php get_footer();?>
